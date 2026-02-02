a29bef97d8fb0565fd8974ca0430997c4cb94831
nassimron <nassimr@outlook.de>
2026-02-02 14:26:38 +0100

    test: add unit tests for Car, Customer and Contract
    # Conflicts:
    #	src/main/java/de/hochschule/carrental/App.java
    #	src/main/java/de/hochschule/test/test.java

--- src/main/java/de/hochschule/carrental/App.java
+++ src/main/java/de/hochschule/carrental/App.java
@@ -15,8 +15,21 @@ import java.sql.SQLException;
  */
 public class App 
 {
+    public static Connection con;
     public static void main( String[] args )
     {
+        TestLogic testlogic = new TestLogic();
+        testlogic.printAvailability();
+
+        try{
+            con = DriverManager.getConnection("jdbc:sqlite:TestDb");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        Test test = new Test();
+
+        test.findCars();
 
 
         System.out.println( "Hello World!" );
