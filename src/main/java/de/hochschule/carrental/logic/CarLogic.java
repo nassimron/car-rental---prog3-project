011c89a39f5f8f6c1d10099e7c33f2568666193b
nassimron <nassimr@outlook.de>
2026-02-02 14:55:21 +0100

    Add CarLogic service with in-memory storage

--- src/main/java/de/hochschule/carrental/logic/CarLogic.java
+++ src/main/java/de/hochschule/carrental/logic/CarLogic.java
@@ -1,23 +1,46 @@
 package de.hochschule.carrental.logic;
 
 import de.hochschule.carrental.data.Car;
+import org.jooq.impl.QOM;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarLogic {
 
+    private List<Car> cars;
+
     public CarLogic(){
+        this.cars = new ArrayList<>();
     }
 
-    private Car create(String ID, String Brand, String Model, String Category, int Price, boolean Availability){
-        return new Car(ID,Brand,Model,Category,Price,Availability);
+    public Car create(String ID, String Brand, String Model, String Category, int Price, boolean Availability){
+
+        Car car = new Car(ID, Brand, Model, Category, Price, Availability);
+        cars.add(car);
+        return car;
     }
 
-    private void delete(Car car){
+    public void delete(String id){
+        cars.removeIf(car -> car.getID().equals(id));
+    }
 
+    public List<Car> getAllCars() {
+        return cars;
+    }
+
+    public Car getCarById(String id) {
+        for(Car car : cars) {
+            if (car.getID().equals(id)) {
+                return car;
+            }
+        }
+        return null;
     }
 
     private void saveToDb(){
 
     }
 
-
 }
+
